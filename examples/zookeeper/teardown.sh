ec198359c244df940b17ac07432ff06ed7bf85b4
Tobias Florek <tob@butter.sh>
2015-09-29 15:10:53 +0200

    ensure pods don't get run on the same node
    Unfortunately that works by allocating a host port, so it won't be nicely separated from the host and needs additional rights. So this also adds a scc and a sa for zookeeper.

--- examples/zookeeper/teardown.sh
+++ examples/zookeeper/teardown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+oc delete scc allowhostports
+oc delete sa zookeeper
 oc delete service zookeeper zookeeper-1 zookeeper-2 zookeeper-3
 oc delete rc zookeeper-1 zookeeper-2 zookeeper-3
 oc delete imageStream zookeeper-346-jdk7
